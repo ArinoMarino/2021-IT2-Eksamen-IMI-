ddc4451b7d67d5c6631ec3ef8cc01b039215f03a
Godlia <52934844+Godlia@users.noreply.github.com>
2022-04-25 15:25:00 +0200

    Squashing bug since 1994
    Holy moly, hvordan er det dere definerer variabler

--- oppgave1/script.js
+++ oppgave1/script.js
@@ -1,12 +1,12 @@
-button = document.getElementsByTagName("button")[0];
-video = document.getElementsByTagName("video")[0];
-playing = false;
+let button = document.getElementsByTagName("button")[0];
+let video = document.getElementsByTagName("video")[0];
+let playing = false;
 
 button.addEventListener("click", playPause);
 
-function playPause(){
+function playPause() {
     console.log("ran")
-    if(playing == false){
+    if (!playing) {
         video.play();
         button.innerHTML = "pause";
         playing = true;
